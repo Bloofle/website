f14d075d26f557f964a39a08c7f837528db1eaf2
Yuriy <yuriy.molchanyuk@wisetechglobal.com>
2021-03-03 20:33:14 +1100

    Add more layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import atlantis from './media/atlantis.jpg';
 import createMemories from './media/creatememories.jpg';
 import bonfire from './media/bonfire.jpg';
 import { Header } from './components/header';
-import { Carousel, CarouselItem } from 'react-bootstrap';
+import { Carousel, CarouselItem, Container } from 'react-bootstrap';
 
 function App() {
   return (
@@ -38,13 +38,71 @@ function App() {
             src={bonfire}
             alt="Third slide"
           />
-
           <Carousel.Caption>
             <h3>Take a seat</h3>
             <p>Relax, sit down and get comfortable.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      <br />
+      <Container className="container marketing">
+        <div className="row">
+          <div className="col-lg-4">
+            <img className="rounded-circle" src={atlantis} alt="Gate to Atlantis" width="140" height="140" />
+            <h2>Gate to Atlantis</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div className="col-lg-4">
+            <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
+            <h2>Mount Fuji View</h2>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div className="col-lg-4">
+            <img className="rounded-circle" src={bonfire} alt="Cliffside Serenity" width="140" height="140" />
+            <h2>Cliffside Serenity</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+        <div className="row featurette">
+          <div className="col-md-7">
+            <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It'll blow your mind.</span></h2>
+            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div className="col-md-5">
+            <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+        <div className="row featurette">
+          <div className="col-md-7 order-md-2">
+            <h2 className="featurette-heading">Oh yeah, it's that good. <span className="text-muted">See for yourself.</span></h2>
+            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div className="col-md-5 order-md-1">
+            <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+
+        <div className="row featurette">
+          <div className="col-md-7">
+            <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
+            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div className="col-md-5">
+            <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+          </div>
+        </div>
+
+        <hr className="featurette-divider" />
+      </Container>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
  return (
     <header>
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a className="navbar-brand" href="#">Carousel</a>
+      <a className="navbar-brand" href="#">Bloofle</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>

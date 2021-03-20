9f834a7c97aafec7592235cf3f80e9cf84e42ab6
Yuriy <molchanyuk.yuriy@gmail.com>
2021-03-20 21:47:33 +1100

    Add some more stuff

--- src/api/AccessChecker/index.ts
+++ src/api/AccessChecker/index.ts
@@ -1,23 +1,40 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+interface apiResponse {
+    name?: string,
+    count: number,
+    imageUrlPrefix: string,
+    error?: string,
+}
+
 const accessChecker: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('Received an authorization request.');
     const accessCode = ((req.query && req.query.accessCode) || (req.body && req.body.accessCode));
     const name = ((req.query && req.query.name) || (req.body && req.body.name));
 
-    if (accessCode === '' || accessCode === null || accessCode !== 'bloofle128600') {
-        context.res = {
-            status: 403 //Forbidden
+    if (accessCode === '' || accessCode === null || accessCode !== process.env.secretCode) {
+        const responseData: apiResponse = {
+            name: name,
+            count: 0,
+            imageUrlPrefix: '',
+            error: 'Sorry, your access Code does not work'
         }
-    }
-    else {
-        const responseMessage = name
-            ? "Hello, " + name + ". Welcome to my secret page!"
-            : "We don't know your name, but you are still welcome";
 
         context.res = {
             status: 200,
-            body: responseMessage
+            body: responseData
+        }
+    }
+    else {
+        const responseData: apiResponse = {
+            name: name,
+            count: 7,
+            imageUrlPrefix: 'https://bloofleblob.file.core.windows.net/awards/George_Awards'
+        }
+
+        context.res = {
+            status: 200,
+            body: responseData
         }
     }
 };

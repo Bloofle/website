411851a1cd8e0b50f8f5c9e2ed5a75ad072543fc
Bloofle <george.molchanyuk@gmail.com>
2021-03-21 20:18:22 +1100

    Fix videos

--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -22,6 +22,9 @@ export const Videos = () => {
             <Creation title="Night Falls 360" video="https://www.youtube.com/embed/oq8G7A09PHs" description="This is my latest creation. It includes an entire playlist of music and wonderful scenery. Luckily, I made a 360 Video of it." link="" />
             <Creation title="Queen Live Aid 1985" video="https://www.youtube.com/embed/6nlRiIcjZ-c" description="Not many people know this, but I am obsessed with Queen. This year I had the opportunity to help in a show called Live Aid. " link="" />
             <Creation title="MountFujiView Trailer" video="https://www.youtube.com/embed/KvYY7NpeRZI" description="This is the trailer to Mount Fuji View. I had a friend create this trailer for me and I love it. " link="" />
+            <Creation title="Creating A Plant Out Of Random Shapes" video="https://www.youtube.com/embed/RTSjP9qclvw" description="Somehow, I managed to make a nice looking plant out of a random shape. This video demonstrates how I did it." link="" />
+            <Creation title="Fortnite" video="https://www.youtube.com/embed/awkp1wdYSGE" description="An introduction to my favourite place in my Fortnite collection." link="" />
+            <Creation title="Creating A Boat In RecRoom" video="https://www.youtube.com/embed/3lEwSGzwzQg" description="Creating a boat in Recroom, this is how I did it." link="" />
             <Row>&nbsp;</Row>
         </Container >
     );

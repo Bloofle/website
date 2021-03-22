ed87dd86f694328e57332062d9d5014c621fc4a2
Bloofle <george.molchanyuk@gmail.com>
2021-03-22 15:13:01 +1100

    Added analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-54RCF6XZ72');
 
 ReactDOM.render(
   <React.StrictMode>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,7 +5,8 @@ import { Videos } from '../pages/videos';
 import { Home } from '../pages/home';
 import { About } from '../pages/about';
 import { VR } from '../pages/vr';
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import { MountFuji } from '../pages/RecRoom/mountfuji';
 import { GateToAtlantis } from '../pages/RecRoom/atlantis';
 import { AJR } from '../pages/RecRoom/ajr';
@@ -15,49 +16,55 @@ import { Fortnite } from '../pages/Fortnite/fortnite';
 import { ModernSuite } from '../pages/RecRoom/modernsuite';
 import { Secret } from '../pages/Secret/secret';
 
-export const Routes = () => (
-    <Switch>
-        <Route path="/drawings">
-            <Drawings />
-        </Route>
-        <Route path="/videos">
-            <Videos />
-        </Route>
-        <Route path="/music">
-            <Music />
-        </Route>
-        <Route path="/about">
-            <About />
-        </Route>
-        <Route exact={true} path="/vr">
-            <VR />
-        </Route>
-        <Route exact={true} path="/vr/mountfuji">
-            <MountFuji />
-        </Route>
-        <Route exact={true} path="/vr/atlantis">
-            <GateToAtlantis />
-        </Route>
-        <Route exact={true} path="/vr/wires">
-            <AJR />
-        </Route>
-        <Route exact={true} path="/vr/serenity">
-            <CliffSideSerenity />
-        </Route>
-        <Route exact={true} path="/vr/modernsuite">
-            <ModernSuite />
-        </Route>
-        <Route path="/secret">
-            <Secret />
-        </Route>
-        <Route path="/minecraft">
-            <Minecraft />
-        </Route>
-        <Route path="/fortnite">
-            <Fortnite />
-        </Route>
-        <Route path="/">
-            <Home />
-        </Route>
-    </Switch>
-);
+export const Routes = () => {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    });
+
+    return (
+        <Switch>
+            <Route path="/drawings">
+                <Drawings />
+            </Route>
+            <Route path="/videos">
+                <Videos />
+            </Route>
+            <Route path="/music">
+                <Music />
+            </Route>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route exact={true} path="/vr">
+                <VR />
+            </Route>
+            <Route exact={true} path="/vr/mountfuji">
+                <MountFuji />
+            </Route>
+            <Route exact={true} path="/vr/atlantis">
+                <GateToAtlantis />
+            </Route>
+            <Route exact={true} path="/vr/wires">
+                <AJR />
+            </Route>
+            <Route exact={true} path="/vr/serenity">
+                <CliffSideSerenity />
+            </Route>
+            <Route exact={true} path="/vr/modernsuite">
+                <ModernSuite />
+            </Route>
+            <Route path="/secret">
+                <Secret />
+            </Route>
+            <Route path="/minecraft">
+                <Minecraft />
+            </Route>
+            <Route path="/fortnite">
+                <Fortnite />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    )
+};
